67cdd633f9b25a4b8ee0907b499879474c9c5710
juliusdw <mail@jdew.de>
2023-04-14 18:42:44 +0200

    rename home to camera

--- app/views/camera.js
+++ app/views/camera.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 
-export default function App() {
+export default function Camera() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
